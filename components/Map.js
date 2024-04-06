342fde8571e45b810fc4f3a35db9acc2c908923b
P-Potdar <prabhatpotdar37@gmail.com>
2024-04-06 12:04:18 +0530

    inital commit

---
+++ components/Map.js
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { useEffect, useContext } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { UberContext } from '../context/uberContext'
+
+const style = {
+  wrapper: `flex-1 h-full w-full`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: 'flex-1 w-full h-full',
+rideRequestContainer: 'h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end'
+
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
+const Map = () => {
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
+
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates)
+    }
+
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates)
+    }
+
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 400,
+      })
+    }
+  }, [pickupCoordinates, dropoffCoordinates])
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
+  return <div className={style.wrapper} id='map' />
+}
+
+export default Map
